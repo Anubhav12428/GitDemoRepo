088a9aaf62c52755bd686e88a49bbee1c5f2743d
Anubhav12428 <133944206+Anubhav12428@users.noreply.github.com>
2023-05-19 09:44:54 +0530

    Modified C3

--- src/pacakage12/C3.java
+++ src/pacakage12/C3.java
@@ -3,6 +3,8 @@ package pacakage12;
 public class C3 {
 
 	public static void main(String[] args) {
-		System.out.println("C3 hii");
+		System.out.println("C3 hii edit in already exist code");
+		System.out.println("C3 hii New data");
+		System.out.println("New Code");
 	}
 }
